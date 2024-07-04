2f4e31ee2561f26ea77d284f366be9b668453ae0
komeegbedi <oghenekomeegbedi222@gmail.com>
2024-07-03 19:16:36 -0500

    new commit

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,19 +1,15 @@
 import UIKit
 import Flutter
-import flutter_plugin_android_lifecycle  // Add this line
+import Firebase
 
 @UIApplicationMain
-class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    if #available(iOS 10.0, *) {
-      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
-    }
-
-    FirebaseApp.configure()  // Add this line if you're using Firebase
-
+    FirebaseApp.configure()
+    GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
