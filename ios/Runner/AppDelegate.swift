b3ac5b585a13f856b8359082b14cde2fcc4bb2ca
Ariyo <osuolaleariyo@gmail.com>
2024-07-08 17:15:05 +0100

    New commit

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,9 +1,6 @@
 import UIKit
 import Flutter
-<<<<<<< HEAD
 import Firebase
-=======
->>>>>>> d394457413a7f53d999f9a91bb7b2d287f06d55c
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -11,10 +8,7 @@ import Firebase
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-<<<<<<< HEAD
     FirebaseApp.configure()
-=======
->>>>>>> d394457413a7f53d999f9a91bb7b2d287f06d55c
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
